58353cc44baa69a02bdaeebdc8860120ad66c10a
Kristijan Husak <husakkristijan@gmail.com>
2014-10-16 00:56:02 +0200

    Adds form facade if it doesn't exist.

--- src/Kris/LaravelFormBuilder/FormBuilderServiceProvider.php
+++ src/Kris/LaravelFormBuilder/FormBuilderServiceProvider.php
@@ -12,6 +12,8 @@ class FormBuilderServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->bindFormIfNeeded();
+
         $this->app->bindShared('Kris/LaravelFormBuilder/FormBuilder', function ($app) {
             $formHelper = new FormHelper($app['view'], $app['config']);
             return new FormBuilder($app['container'], $formHelper);
@@ -27,5 +29,23 @@ class FormBuilderServiceProvider extends ServiceProvider
     public function provides()
     {
         return ['laravel-form-builder'];
+    }
+
+    private function bindFormIfNeeded()
+    {
+        if (!$this->app['form']) {
+            $this->app->bindShared('form', function($app)
+            {
+                $form = new FormBuilder($app['html'], $app['url'], $app['session.store']->getToken());
+
+                return $form->setSessionStore($app['session.store']);
+            });
+
+            AliasLoader::getInstance()->alias(
+                'Form',
+                'Illuminate\Html\FormFacade'
+            );
+        }
+
     }
 }
