ee6c77c86b6b987bdd3d00482ee3af143b336124
Kristijan Husak <husakkristijan@gmail.com>
2014-10-28 10:45:22 +0100

    Update checkbox and radio views.

--- src/views/checkbox.php
+++ src/views/checkbox.php
@@ -2,16 +2,16 @@
 <div <?= $options['wrapperAttrs'] ?> >
 <?php endif; ?>
 
+    <?php if ($showField): ?>
+        <?= Form::checkbox($name, $options['default_value'], $options['checked'], $options['attr']) ?>
+    <?php endif; ?>
+
     <?php if ($showLabel): ?>
         <?php if ($options['is_child']): ?>
             <label <?= $options['labelAttrs'] ?>><?= $options['label'] ?></label>
         <?php else: ?>
             <?= Form::label($name, $options['label'], $options['label_attr']) ?>
         <?php endif; ?>
-    <?php endif; ?>
-
-    <?php if ($showField): ?>
-    <?= Form::checkbox($name, $options['default_value'], $options['checked'], $options['attr']) ?>
     <?php endif; ?>
 
     <?php if ($showError && isset($errors)): ?>
