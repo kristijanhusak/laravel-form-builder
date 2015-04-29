23f524d2180863e228e129646b4de322f5ea6c96
Kristijan Husak <husakkristijan@gmail.com>
2015-04-29 14:25:52 +0200

    Add wrapper for button type.

--- src/views/button.php
+++ src/views/button.php
@@ -1,6 +1,14 @@
+<?php if ($options['wrapper'] !== false): ?>
+<div <?= $options['wrapperAttrs'] ?> >
+<?php endif; ?>
+
 <?= Form::button($options['label'], $options['attr']) ?>
 <?php if ($options['help_block']['text']): ?>
     <<?= $options['help_block']['tag'] ?> <?= $options['help_block']['helpBlockAttrs'] ?>>
         <?= $options['help_block']['text'] ?>
     </<?= $options['help_block']['tag'] ?>>
+<?php endif; ?>
+
+<?php if ($options['wrapper'] !== false): ?>
+</div>
 <?php endif; ?>
