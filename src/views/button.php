9b2a8dddb4ff99766cdfd8ca4b85b1ae2e691a1a
Kristijan Husak <husakkristijan@gmail.com>
2015-09-22 13:12:40 +0200

    Fix buttons not showing.

--- src/views/button.php
+++ src/views/button.php
@@ -2,6 +2,7 @@
 <div <?= $options['wrapperAttrs'] ?> >
 <?php endif; ?>
 
+<?= Form::button($options['label'], $options['attr']) ?>
 <?php include 'help_block.php' ?>
 
 <?php if ($options['wrapper'] !== false): ?>
