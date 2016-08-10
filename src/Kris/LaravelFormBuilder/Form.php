9d583578ea5253a8e1ef92baffd8f7b6d7f61d31
Rudie Dirkx <github@hotblocks.nl>
2016-08-10 20:10:43 +0200

    #250 +getFieldName() nested check

--- src/Kris/LaravelFormBuilder/Form.php
+++ src/Kris/LaravelFormBuilder/Form.php
@@ -997,12 +997,17 @@ class Form
      */
     protected function getFieldName($name)
     {
-        if ($this->getName() !== null) {
-            return $this->formHelper->transformToBracketSyntax(
-                $this->formHelper->transformToDotSyntax(
-                    $this->getName() . '[' . $name . ']'
-                )
-            );
+        $formName = $this->getName();
+        if ($formName !== null) {
+            if (strpos($formName, '[') !== false || strpos($formName, ']') !== false) {
+                return $this->formHelper->transformToBracketSyntax(
+                    $this->formHelper->transformToDotSyntax(
+                        $formName . '[' . $name . ']'
+                    )
+                );
+            }
+
+            return $formName . '[' . $name . ']';
         }
 
         return $name;
