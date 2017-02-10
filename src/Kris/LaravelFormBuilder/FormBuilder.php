0744492196674ba65710a491715fb0f953770b39
saeidraei <saeid.raei@hotmail.com>
2017-02-10 12:51:20 +0330

    adds createByArray function to formBuilder class

--- src/Kris/LaravelFormBuilder/FormBuilder.php
+++ src/Kris/LaravelFormBuilder/FormBuilder.php
@@ -1,6 +1,4 @@
-<?php
-
-namespace Kris\LaravelFormBuilder;
+<?php namespace Kris\LaravelFormBuilder;
 
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Events\Dispatcher as EventDispatcher;
@@ -25,9 +23,8 @@ class FormBuilder
     protected $eventDispatcher;
 
     /**
-     * @param Container  $container
+     * @param Container $container
      * @param FormHelper $formHelper
-     * @param EventDispatcher $eventDispatcher
      */
     public function __construct(Container $container, FormHelper $formHelper, EventDispatcher $eventDispatcher)
     {
@@ -37,11 +34,9 @@ class FormBuilder
     }
 
     /**
-     * Create a Form instance.
-     *
-     * @param string $formClass The name of the class that inherits \Kris\LaravelFormBuilder\Form.
-     * @param array $options|null
-     * @param array $data|null
+     * @param       $formClass
+     * @param       $options
+     * @param       $data
      * @return Form
      */
     public function create($formClass, array $options = [], array $data = [])
@@ -72,7 +67,54 @@ class FormBuilder
     }
 
     /**
-     * Get the namespace from the config.
+     * @param $items
+     * @param array $options
+     * @param array $data
+     * @return mixed
+     */
+    public function createByArray($items, array $options = [], array $data = [])
+    {
+        $form = $this->container
+            ->make(Form::class)
+            ->addData($data)
+            ->setRequest($this->container->make('request'))
+            ->setFormHelper($this->formHelper)
+            ->setEventDispatcher($this->eventDispatcher)
+            ->setFormBuilder($this)
+            ->setValidator($this->container->make('validator'))
+            ->setFormOptions($options);
+
+        $this->buildFormByArray($form, $items);
+
+        $this->eventDispatcher->fire(new AfterFormCreation($form));
+
+        return $form;
+    }
+
+    /**
+     * @param $form
+     * @param $items
+     */
+    public function buildFormByArray($form, $items)
+    {
+        foreach ($items as $item) {
+            if (!isset($item['name'])) {
+                throw new \InvalidArgumentException(
+                    'Name is not set in form array.'
+                );
+            }
+            $name = $item['name'];
+            $type = isset($item['type']) && $item['type'] ? $item['type'] : '';
+            $modify = isset($item['modify']) && $item['modify'] ? $item['modify'] : false;
+            unset($item['name']);
+            unset($item['type']);
+            unset($item['modify']);
+            $form->add($name, $type, $item, $modify);
+        }
+    }
+
+    /**
+     * Get the namespace from the config
      *
      * @return string
      */
@@ -88,11 +130,11 @@ class FormBuilder
     }
 
     /**
-     * Get instance of the empty form which can be modified.
+     * Get instance of the empty form which can be modified
      *
      * @param array $options
      * @param array $data
-     * @return \Kris\LaravelFormBuilder\Form
+     * @return Form
      */
     public function plain(array $options = [], array $data = [])
     {
