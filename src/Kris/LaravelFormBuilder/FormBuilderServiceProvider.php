5accf3c8d4e464f3ef0290eb8c9cdc3a85af5342
Rudie Dirkx <github@hotblocks.nl>
2022-03-03 22:43:50 +0100

    Don't register laravelcollective/html services anymore

--- src/Kris/LaravelFormBuilder/FormBuilderServiceProvider.php
+++ src/Kris/LaravelFormBuilder/FormBuilderServiceProvider.php
@@ -21,9 +21,6 @@ class FormBuilderServiceProvider extends ServiceProvider
     {
         $this->commands('Kris\LaravelFormBuilder\Console\FormMakeCommand');
 
-        $this->registerHtmlIfNeeded();
-        $this->registerFormIfHeeded();
-
         $this->mergeConfigFrom(
             __DIR__ . '/../../config/config.php',
             'laravel-form-builder'
@@ -100,63 +97,6 @@ class FormBuilderServiceProvider extends ServiceProvider
     public function provides()
     {
         return ['laravel-form-builder'];
-    }
-
-    /**
-     * Add Laravel Form to container if not already set.
-     *
-     * @return void
-     */
-    private function registerFormIfHeeded()
-    {
-        if (!$this->app->offsetExists('form')) {
-
-            $this->app->singleton('form', function($app) {
-                $form = new LaravelForm($app[ 'html' ], $app[ 'url' ], $app[ 'view' ], $app[ 'session.store' ]->token());
-
-                return $form->setSessionStore($app['session.store']);
-            });
-
-            if (! $this->aliasExists('Form')) {
-
-                AliasLoader::getInstance()->alias(
-                    'Form',
-                    'Collective\Html\FormFacade'
-                );
-            }
-        }
-    }
-
-    /**
-     * Add Laravel Html to container if not already set.
-     */
-    private function registerHtmlIfNeeded()
-    {
-        if (!$this->app->offsetExists('html')) {
-
-            $this->app->singleton('html', function($app) {
-                return new HtmlBuilder($app['url'], $app['view']);
-            });
-
-            if (! $this->aliasExists('Html')) {
-
-                AliasLoader::getInstance()->alias(
-                    'Html',
-                    'Collective\Html\HtmlFacade'
-                );
-            }
-        }
-    }
-
-    /**
-     * Check if an alias already exists in the IOC.
-     *
-     * @param string $alias
-     * @return bool
-     */
-    private function aliasExists($alias)
-    {
-        return array_key_exists($alias, AliasLoader::getInstance()->getAliases());
     }
 
 }
--- tests/FormBuilderTestCase.php
+++ tests/FormBuilderTestCase.php
@@ -139,13 +139,18 @@ abstract class FormBuilderTestCase extends TestCase {
 
     protected function getPackageProviders($app)
     {
-        return ['Kris\LaravelFormBuilder\FormBuilderServiceProvider'];
+        return [
+            'Collective\Html\HtmlServiceProvider',
+            'Kris\LaravelFormBuilder\FormBuilderServiceProvider',
+        ];
     }
 
     protected function getPackageAliases($app)
     {
         return [
-            'Acme' => 'Kris\LaravelFormBuilder\Facades\FormBuilder'
+            'Acme' => 'Kris\LaravelFormBuilder\Facades\FormBuilder',
+            'Form' => 'Collective\Html\FormFacade',
+            'Html' => 'Collective\Html\HtmlFacade',
         ];
     }
 
