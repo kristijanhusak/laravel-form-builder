84d634701faaefe76d3adc702c2544f8273aec80
Chris <chris.kamperman@timeedit.com>
2026-08-04 18:28:02 +0200

    Remove deprecated method call;

--- tests/Fields/FormFieldTest.php
+++ tests/Fields/FormFieldTest.php
@@ -413,7 +413,6 @@ class FormFieldTest extends FormBuilderTestCase
 
         $reflection = new \ReflectionClass($plainForm);
         $method = $reflection->getMethod($methodName);
-        $method->setAccessible(true);
 
         $this->assertTrue($method->invokeArgs($plainForm, []));
         $this->assertFalse($method->invokeArgs($modifiedForm, []));
@@ -430,8 +429,6 @@ class FormFieldTest extends FormBuilderTestCase
 
         $reflection = new \ReflectionClass($customPlainForm);
         $method = $reflection->getMethod($methodName);
-        $method->setAccessible(true);
-
 
         $this->assertTrue($method->invokeArgs($customPlainForm, []));
     }
